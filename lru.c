d474031e05d931a2741139ca62c7ad3801860cfd
diego <diego@DIEGO>
2025-10-20 19:35:12 -0300

    Correcciones sobre el modo de uso

--- lru.c
+++ lru.c
@@ -1,39 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+
+
+void menu();
 
 int main(int argc, char* argv[]) 
 {
-	int n;
-	char letra;
+	int n, ejecutando=1, contador=0;
+	char letra, comando[7];
 
-	if (strcmp(argv[1], "create") == 0) 
+	menu();
+
+	while (ejecutando)
 	{
-		n = atoi(argv[2]);
-		printf("\ncache creado con tamanyo: %d\n", n);
-	}
+		printf("lru> ");
+		scanf("%s", comando);
 
-	if (strcmp(argv[1], "add") == 0)
-	{
-		letra = argv[2][0];
-		printf("\nLetra %c anyadida al cache\n", letra);
-	}
+		if (strcmp(comando, "create") == 0)
+		{
+			if (contador == 0)
+			{
+				scanf("%d", &n);
+				printf("\nCache creado con tamanyo: %d\n", n);
+				contador = 1;
+			}
+			else
+			{
+				printf("\nEl cache ya ha sido creado\n");
+			}
+		}
 
-	if (strcmp(argv[1], "all") == 0)
-	{
-		printf("\nMostrando el contenido del cache: \n");
-	}
+		if (strcmp(comando, "add") == 0)
+		{
+			scanf(" %c", &letra);
+			printf("\nLetra %c anyadida al cache\n", letra);
+		}
 
-	if (strcmp(argv[1], "get") == 0)
-	{
-		letra = argv[2][0];
-		printf("\nSe ha utilizado: %c\n", letra);
-	}
+		if (strcmp(comando, "all") == 0)
+		{
+			printf("\nMostrando el contenido del cache: \n");
+		}
 
-	if (strcmp(argv[1], "exit") == 0)
-	{
-		printf("\nLiberando cache y saliendo del programa\n");
-	}
+		if (strcmp(comando, "get") == 0)
+		{
+			scanf(" %c", &letra);
+			printf("\nSe ha utilizado: %c\n", letra);
+		}
 
+		if (strcmp(comando, "exit") == 0)
+		{
+			printf("\nLiberando cache y saliendo del programa\n");
+			ejecutando = 0;
+		}
+	}
 	return 0;
+}
+
+void menu()
+{
+	printf("\nComandos disponibles:\n");
+	printf("create <n> : Crea un cache de tamanyo n\n");
+	printf("add <x>    : Anyade el elemento x al cache\n");
+	printf("get <x>    : Utiliza el elemento x del cache\n");
+	printf("all        : Muestra el contenido del cache\n");
+	printf("exit       : Libera el cache y sale del programa\n\n");
 }
