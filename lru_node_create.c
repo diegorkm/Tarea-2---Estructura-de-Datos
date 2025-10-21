ac7fd7ada064ee58479fa29178d8588c39483aa1
SebRamc <138082698+SebRamc@users.noreply.github.com>
2025-10-21 19:55:24 -0300

    Mezcla de lru_create.c con lru_node.c

--- lru_node_create.c
+++ lru_node_create.c
@@ -40,3 +40,23 @@ LRUCache* create_cache(int capacity){
 
     return cache;
 }
+
+/**
+ * @brief Crea un nuevo nodo (Node) para almacenar un dato de tipo char.
+ * * @param data El carácter (letra mayúscula) que se almacenará en el nodo.
+ * @return Node* Puntero al nodo creado, o NULL si falla la asignación de memoria.
+ */
+
+Node* create_node(char data) 
+{
+    Node* new_node = (Node*)malloc(sizeof(Node));
+    if (new_node == NULL) 
+    {
+        printf("Error al asignar memoria para el nodo.\n");
+        return NULL;
+    }
+    new_node->data = data;
+    new_node->prev = NULL;
+    new_node->next = NULL;
+    return new_node;
+}
