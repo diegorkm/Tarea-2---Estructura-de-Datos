61e72d0a175252c2fed9b4de0100af02a4cb3589
diegorkm <diegoaperaltar@gmail.com>
2025-10-21 22:16:31 -0300

    Delete add.c

--- add.c
+++
@@ -1,9 +0,0 @@
-#include "lru.h"
-void add_data(LRUCache* cache, char data) 
-{
-    if (cache == NULL || cache->capacity == 0) 
-    {
-        printf("Error: El cache no ha sido inicializado.\n");
-        return;
-    }
-}
